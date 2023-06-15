9227fd7a5f05d51879be0593c57ac7a8bce236ca
Landon Connell <landonbconnell2001@gmail.com>
2023-06-15 12:44:40 -0400

    added some more windows to OrganizeLikedSongs feature

--- client/src/features/OrganizeLikedSongs.js
+++ client/src/features/OrganizeLikedSongs.js
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { CircularProgress, Typography, Box, List, ListItem, ListItemText, Checkbox, FormControlLabel } from '@mui/material';
+import { CircularProgress, Typography, Box, List, ListItem, ListItemText, Checkbox, FormControlLabel, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
-const OrganizeLikedSongs = () => {
-    const [tracks, setTracks] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [selectedTracks, setSelectedTracks] = useState([]);
-    const [selectAll, setSelectAll] = useState(false);
-
+const FetchTracks = (setTracks, setLoading) => {
     useEffect(() => {
         axios.get('http://localhost:5000/tracks/saved')
             .then(response => {
@@ -19,6 +15,179 @@ const OrganizeLikedSongs = () => {
                 setLoading(false);
             })
     }, []) // only run this hook once, when the component first loads
+}
+
+const LoadingComponent = () => (
+    <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '50vh' }}>
+        <CircularProgress sx={{ color: '#1DB954' }} />
+        <Typography variant="h6" style={{ marginTop: '20px' }}>Getting your Liked Songs</Typography>
+    </Box>
+);
+
+const OrganizeLikedSongsHeader = () => (
+    <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center" // align box to center
+        justifyContent="center"
+        style={{ 
+            backgroundColor: '#1DB954', 
+            color: 'black', 
+            padding: '10px 0', 
+            borderRadius: '10px 10px 0 0',
+            position: 'fixed', // Change this from 'sticky' to 'fixed'
+            top: 0, 
+            left: '40px',
+            right: '40px',
+            zIndex: 999, // Ensure the box appears over the content
+            marginTop: '30px'
+        }} 
+    >
+        <Typography variant="h3" style={{ marginBottom: '20px', marginTop:'20px', color: 'white' }}>Organize Liked Songs</Typography>
+    </Box>
+);
+
+const TrackList = ({ tracks, selectedTracks, handleCheck }) => (
+    <List>
+        {tracks.map(track => (
+            <ListItem key={track.id} divider>
+                <ListItemText 
+                    primary={track.name} 
+                    secondary={<Typography variant="body2" style={{ color: 'gray' }}>{track.artist}</Typography>} 
+                />
+                <Checkbox 
+                    checked={selectedTracks.includes(track)}
+                    color="default" 
+                    inputProps={{ 'aria-label': 'checkbox with default color' }} 
+                    onChange={() => handleCheck(track)}
+                    disableRipple
+                    size="large"
+                    style={{ color: 'white' }}
+                />
+            </ListItem>
+        ))}
+    </List>
+);
+
+const SelectAll = ({ selectAll, handleSelectAll }) => (
+    <FormControlLabel
+        control={<Checkbox checked={selectAll} onChange={handleSelectAll} style={{ color: 'white' }} disableRipple size="large"/>}
+        label="Select All"
+        labelPlacement="start"
+        style={{ color: 'white' }}
+    />
+);
+
+const GeneratePlaylistBox = ({ playlists, onDelete }) => (
+    <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        style={{
+            maxHeight: '80vh',
+            overflow: 'auto',
+            width: '100%',
+            maxWidth: '800px', // increased max-width
+            backgroundColor: '#282828',
+            padding: '0 15px 0 15px',
+            borderRadius: '0 0 0 10px',
+            marginLeft: '40px',
+            marginRight: '40px'
+        }}
+    >
+        <button style={{ color: 'white', backgroundColor: '#1DB954', padding: '10px 20px', borderRadius: '5px', border: 'none' }}>Generate Playlist Names</button>
+        {playlists.map((playlist) => (
+            <PlaylistNameAndDescription 
+                key={playlist.id}
+                playlistName={playlist.name} 
+                playlistDescription={playlist.description} 
+                onDelete={() => onDelete(playlist.id)}
+            />
+        ))}
+    </Box>
+);
+
+const PlaylistBox = () => (
+    <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        style={{
+            maxHeight: '80vh',
+            overflow: 'auto',
+            width: '100%',
+            maxWidth: '800px', // increased max-width
+            backgroundColor: '#282828',
+            paddingLeft: '15px',
+            borderRadius: '0 0 0 10px',
+            marginLeft: '40px',
+            marginRight: '40px'
+        }}
+    >
+        <Typography variant="h5" style={{ color: 'white', padding: '15px 0', borderBottom: '1px solid gray' }}>Playlists</Typography>
+    </Box>
+);
+
+const PlaylistNameAndDescription = ({ playlistName, playlistDescription, onDelete }) => {
+    const [name, setName] = useState(playlistName);
+    const [description, setDescription] = useState(playlistDescription);
+  
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            position="relative"
+            padding="15px"
+            margin="0 0 15px 0" // Removed horizontal margins
+            bgcolor="#3f3f3f"
+            borderRadius="5px"
+            width="100%" // Full width of the parent
+        >
+            <IconButton
+                aria-label="delete"
+                onClick={onDelete}
+                style={{ position: 'absolute', top: '10px', right: '10px', color: 'white' }}
+            >
+                <CloseIcon />
+            </IconButton>
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                style={{ color: 'white', fontSize: '1.25rem', width: '100%', marginBottom: '10px', backgroundColor: 'transparent', border: 'none' }}
+            />
+            <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                style={{ color: 'white', fontSize: '1rem', width: '100%', backgroundColor: 'transparent', border: 'none' }}
+            />
+        </Box>
+    );
+};
+
+
+
+// Main Component
+const OrganizeLikedSongs = () => {
+    const [tracks, setTracks] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [selectedTracks, setSelectedTracks] = useState([]);
+    const [selectAll, setSelectAll] = useState(false);
+    const [playlists, setPlaylists] = useState([
+        { id: 1, name: "Sample Playlist 1", description: "This is a sample playlist 1." },
+        { id: 2, name: "Sample Playlist 2", description: "This is a sample playlist 2." }
+    ]);
+    
+
+    FetchTracks(setTracks, setLoading);
 
     const handleCheck = (track) => {
         if (selectedTracks.find((selectedTrack) => selectedTrack.id === track.id)) {
@@ -37,6 +206,10 @@ const OrganizeLikedSongs = () => {
         }
     }
 
+    const handleDeletePlaylist = (id) => {
+        setPlaylists(playlists.filter((playlist) => playlist.id !== id));
+    };    
+
     return (
         <Box
             display="flex"
@@ -51,48 +224,23 @@ const OrganizeLikedSongs = () => {
              }}
         >
             {!loading && 
-                <Box
-                    display="flex"
-                    flexDirection="column"
-                    alignItems="center" // align box to center
-                    justifyContent="center"
-                    style={{ 
-                        backgroundColor: '#1DB954', 
-                        color: 'black', 
-                        padding: '10px 0', 
-                        borderRadius: '10px 10px 0 0',
-                        position: 'fixed', // Change this from 'sticky' to 'fixed'
-                        top: 0, 
-                        left: '40px',
-                        right: '40px',
-                        zIndex: 999, // Ensure the box appears over the content
-                        marginTop: '30px'
-                    }} 
-                >
-                    <Typography variant="h3" style={{ marginBottom: '20px', marginTop:'20px', color: 'white' }}>Organize Liked Songs</Typography>
-                </Box>
+                <OrganizeLikedSongsHeader />
             }
             <Box style={{ paddingTop: !loading ? '100px' : '0' }}>
               {loading 
-              ? <Box
-                  display="flex"
-                  flexDirection="column"
-                  alignItems="center"
-                  justifyContent="center"
-                  style={{ minHeight: '50vh' }}>
-                  <CircularProgress sx={{ color: '#1DB954' }} />
-                  <Typography variant="h6" style={{ marginTop: '20px' }}>Getting your Liked Songs</Typography>
-                </Box>
-              : <Box
-                  style={{
-                      maxHeight: '80vh',
-                      overflow: 'auto',
-                      width: '100%',
-                      maxWidth: '600px',
-                      backgroundColor: '#282828',
-                      paddingLeft: '15px',
-                      borderRadius: '0 0 0 10px'
-                  }}>
+              ? <LoadingComponent />
+              : <Box display="flex" flexDirection="row" justifyContent="space-between">
+                  <Box
+                      style={{
+                          maxHeight: '80vh',
+                          overflow: 'auto',
+                          width: '100%',
+                          maxWidth: '600px',
+                          backgroundColor: '#282828',
+                          paddingLeft: '15px',
+                          borderRadius: '0 0 0 10px'
+                      }}
+                  >
                     <Box 
                         display="flex" 
                         justifyContent="space-between" 
@@ -111,32 +259,12 @@ const OrganizeLikedSongs = () => {
                         }}
                     >
                       <Typography variant="h5" style={{ color: 'white' }}>Liked Songs</Typography>
-                      <FormControlLabel
-                          control={<Checkbox checked={selectAll} onChange={handleSelectAll} style={{ color: 'white' }} disableRipple size="large"/>}
-                          label="Select All"
-                          labelPlacement="start"
-                          style={{ color: 'white' }}
-                      />
+                      <SelectAll selectAll={selectAll} handleSelectAll={handleSelectAll} />
+                    </Box>
+                    <TrackList tracks={tracks} selectedTracks={selectedTracks} handleCheck={handleCheck} />
                   </Box>
-                  <List>
-                      {tracks.map(track => (
-                          <ListItem key={track.id} divider>
-                              <ListItemText 
-                                  primary={track.name} 
-                                  secondary={<Typography variant="body2" style={{ color: 'gray' }}>{track.artist}</Typography>} 
-                              />
-                              <Checkbox 
-                                  checked={selectedTracks.includes(track)}
-                                  color="default" 
-                                  inputProps={{ 'aria-label': 'checkbox with default color' }} 
-                                  onChange={() => handleCheck(track)}
-                                  disableRipple
-                                  size="large"
-                                  style={{ color: 'white' }}
-                              />
-                          </ListItem>
-                      ))}
-                  </List>
+                  <GeneratePlaylistBox playlists={playlists} onDelete={handleDeletePlaylist} />
+                  <PlaylistBox />
                 </Box>
               }
             </Box>
