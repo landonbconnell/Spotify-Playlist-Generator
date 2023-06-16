ef69684c82dfdf4f15f21fbbcc9622f2e9f3c671
Landon Connell <lconnell@revlocal.com>
2023-06-16 14:52:29 -0400

    generate playlist ideas styling and functionality -- commit before major refactor of organize liked songs feature

--- client/src/features/OrganizeLikedSongs.js
+++ client/src/features/OrganizeLikedSongs.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { CircularProgress, Typography, Box, List, ListItem, ListItemText, Checkbox, FormControlLabel, IconButton } from '@mui/material';
+import { CircularProgress, Typography, Box, List, ListItem, ListItemText, Checkbox, FormControlLabel, IconButton, Button } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
-import { Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 const FetchTracks = (setTracks, setLoading) => {
     useEffect(() => {
@@ -20,7 +21,7 @@ const FetchTracks = (setTracks, setLoading) => {
 
 // Function to fetch playlist names and descriptions
 const fetchPlaylistNamesAndDescriptions = (setPlaylists) => {
-    axios.get('http://localhost:5000/playlists/getNamesAndDescriptions')
+    return axios.get('http://localhost:5000/playlists/getNamesAndDescriptions')
         .then(response => {
             setPlaylists(response.data);
         })
@@ -28,6 +29,7 @@ const fetchPlaylistNamesAndDescriptions = (setPlaylists) => {
             console.log(error);
         });
 }
+
 
 const LoadingComponent = () => (
     <Box
@@ -95,42 +97,120 @@ const SelectAll = ({ selectAll, handleSelectAll }) => (
     />
 );
 
-const GeneratePlaylistBox = ({ playlists, setPlaylists, onDelete }) => (
+const GeneratePlaylistBox = ({ playlists, setPlaylists, onDelete }) => {
+    const [keywords, setKeywords] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    console.log(playlists)
+
+    const handleGeneratePlaylistNames = () => {
+        setLoading(true); // Start loading
+        fetchPlaylistNamesAndDescriptions(setPlaylists).finally(() => setLoading(false)); // End loading after fetching
+    }
+
+    return (
+    
     <Box
         display="flex"
         flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
+        justifyContent="start"
+        alignItems="start"
         style={{
             maxHeight: '80vh',
             overflow: 'auto',
             width: '100%',
             maxWidth: '800px',
             backgroundColor: '#282828',
-            padding: '0 15px 0 15px',
-            borderRadius: '0 0 0 10px',
-            marginLeft: '40px',
-            marginRight: '40px'
+            padding: '15px 15px 15px 15px',
+            borderRadius: '0 10px 0 10px',
+            marginLeft: '60px',
+            marginRight: '60px'
         }}
     >
-        <Button
-            variant="contained"
-            color="primary"
-            style={{ marginBottom: '10px' }} // Add some spacing
-            onClick={() => fetchPlaylistNamesAndDescriptions(setPlaylists)}
+        <Box style={{ width: '100%', borderBottom: '1px solid gray', paddingBottom: '20px' }}>
+            <Typography variant="h5" style={{ color: 'white', padding: '10px 0 0 15px' }}>Playlist Idea Generation</Typography>
+        </Box>
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center" // center items along the main axis
+            style={{
+                overflow: 'auto',
+                flex: '1 1 auto',
+                width: '100%',
+                paddingTop: '15px'
+            }}
         >
-            Generate Playlist Names
-        </Button>
-        {playlists.map((playlist) => (
-            <PlaylistNameAndDescription 
-                key={playlist.id}
-                playlistName={playlist.name} 
-                playlistDescription={playlist.description} 
-                onDelete={() => onDelete(playlist.id)}
-            />
-        ))}
-    </Box>
-);
+            { (playlists.length === 0 && loading) &&
+                <>
+                    <CircularProgress sx={{ color: '#1DB954' }} />
+                    <Typography variant="h6" style={{ marginTop: '20px' }}>Generating Playlist Ideas...</Typography>
+                    <Typography variant="p">This may take a minute</Typography>
+                </>
+            }  
+
+            { (playlists.length === 0 && !loading) &&
+                <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ marginBottom: '10px', backgroundColor: '#1DB954' }} // Add some spacing
+                    onClick={handleGeneratePlaylistNames}
+                    disabled={loading} // disable the button when loading
+                >
+                    Generate Playlist Ideas
+                </Button>
+            }
+
+            {playlists.map((playlist) => (
+                <PlaylistNameAndDescription 
+                    key={playlist.id}
+                    playlistName={playlist.name} 
+                    playlistDescription={playlist.description} 
+                    onDelete={() => onDelete(playlist.id)}
+                />
+            ))}
+        </Box>
+
+        {playlists.length !== 0 &&
+            <Box 
+                display="flex"
+                flexDirection="row"
+                justifyContent="center"
+                alignItems="center"
+                style={{ marginTop: '10px', position: 'sticky', bottom: 0, background: '#282828', padding: '20px' }} 
+            >
+                <input 
+                    type="text"
+                    value={keywords}
+                    onChange={(e) => setKeywords(e.target.value)}
+                    placeholder="keyword(s)"
+                    style={{ marginRight: '10px', width: '300px', height: '100%', borderRadius: '5px' }} // Add some spacing
+                />
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ backgroundColor: '#1DB954', marginRight: '10px' }}
+                    onClick={() => { /* Handle add keyword function here */ }}
+                >
+                    <AddIcon />
+                </Button>
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ backgroundColor: '#1DB954' }}
+                    onClick={() => { /* Handle redo function here */ }}
+                >
+                    <RefreshIcon />
+                </Button>
+            </Box>
+        }
+        </Box>
+    )
+}
+
 
 
 const PlaylistBox = () => (
@@ -145,9 +225,7 @@ const PlaylistBox = () => (
             maxWidth: '800px', // increased max-width
             backgroundColor: '#282828',
             paddingLeft: '15px',
-            borderRadius: '0 0 0 10px',
-            marginLeft: '40px',
-            marginRight: '40px'
+            borderRadius: '0 0 0 10px'
         }}
     >
         <Typography variant="h5" style={{ color: 'white', padding: '15px 0', borderBottom: '1px solid gray' }}>Playlists</Typography>
@@ -207,14 +285,8 @@ const OrganizeLikedSongs = () => {
     const [selectedTracks, setSelectedTracks] = useState([]);
     const [selectAll, setSelectAll] = useState(false);
     const [playlists, setPlaylists] = useState([]);
-    
 
     FetchTracks(setTracks, setLoading);
-
-    // Use effect for fetching playlist names and descriptions
-    useEffect(() => {
-        fetchPlaylistNamesAndDescriptions(setPlaylists);
-    }, [setPlaylists]);
 
     const handleCheck = (track) => {
         if (selectedTracks.find((selectedTrack) => selectedTrack.id === track.id)) {
--- server/controllers/playlistController.js
+++ server/controllers/playlistController.js
@@ -8,10 +8,10 @@ const getNamesAndDescriptions = async (req, res) => {
             {
                 model: 'gpt-3.5-turbo',
                 messages: [
-                    { role: 'system', content: "You are an assistant that ONLY speaks JSON and does NOT speak natural language. Each object in the array should contain three attributes: id, playListName, and description. Do not write normal text." },
-                    { role: 'user', content: "I'm trying to organize a list of songs into playlist. Based on some common moods/emotions that music tries to evoke in its listeners, generate 2 playlist names and corresponding descriptions." }
+                    { role: 'system', content: "You are an assistant that ONLY speaks JSON. Return a aingle JSON array of objects. Each object represents a playlist and should have the properties 'id', 'name', and 'description'. The 'id' should be a number, 'name' should be the playlist name, and 'description' should describe the kind of music or mood of the playlist. DO NOT return any text outside the JSON array. The very first character should be '[' and the very last character should be ']'. You are not permitted to return introductions, explanations, or any other non-JSON text." },
+                    { role: 'user', content: "I'm trying to organize a list of songs into playlists. Based on some common moods/emotions that music tries to evoke in its listeners, generate 5 creative and unique playlist names and corresponding descriptions in JSON format." }
                 ],
-                temperature: 1
+                temperature: 0.8
             }, 
             { 
                 headers: {
@@ -21,7 +21,19 @@ const getNamesAndDescriptions = async (req, res) => {
             }
         )
 
-        res.json(JSON.parse(response.data.choices[0].message.content));  // send response to client/
+        let str = response.data.choices[0].message.content;
+        const start = str.indexOf('[');
+        const end = str.indexOf(']');
+    
+        console.log(str)
+
+        if (start !== -1 && end !== -1 && start < end) {
+            str = str.substring(start, end + 1);
+        } else {
+            str = [];
+        }
+
+        res.json(JSON.parse(str));  // send response to client/
     } catch (error) {
         console.error(error);  // log error for debugging
         res.status(500).json({ error: error.toString() });  // send error to client
