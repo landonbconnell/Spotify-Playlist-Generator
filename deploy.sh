a999793c99fb412cbbba9e9cb81485d07bb8dec6
Landon Connell <landonbconnell2001@gmail.com>
2023-06-29 11:12:13 -0400

    modified workflow so containers deploy to ACI instead of Web App for Containers

--- deploy.sh
+++
@@ -1,1 +0,0 @@
-az group create --name SPGResources --location "East US" && az appservice plan create --name SPGPlan --resource-group SPGResources --sku S1 --is-linux && az webapp create --resource-group SPGResources --plan SPGPlan --name SPG-WebApp --multicontainer-config-type compose --multicontainer-config-file docker-compose.yml
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,46 +1,49 @@
 const querystring = require('querystring');
-const axios = require('axios')
-const config = require('../config')
+const axios = require('axios');
+const config = require('../config');
 
 // Your application's client ID and redirect URI
 const client_id = 'be4c953fbc71461a85c3dbe065a32fb4';
-const client_secret = 'd5160ab0275046aebc84e959d596ac64'
-const redirect_uri = 'http://localhost:3000/home';
-const scope = 'user-library-read';  // Modify the scopes as needed
+const client_secret = 'd5160ab0275046aebc84e959d596ac64';
+const redirect_uri = 'http://localhost:80/home';
+const scope = 'user-library-read'; // Modify the scopes as needed
 
-const authRequest = ((req, res) => {
-    res.redirect('https://accounts.spotify.com/authorize?' +
+const authRequest = (req, res) => {
+  res.redirect(
+    'https://accounts.spotify.com/authorize?' +
       querystring.stringify({
         response_type: 'code',
         client_id,
         scope,
-        redirect_uri
-    }));
-})
+        redirect_uri,
+      })
+  );
+};
 
 const getAccessToken = async (req, res) => {
   try {
-      const response = await axios.post('https://accounts.spotify.com/api/token', 
-        querystring.stringify({
-          grant_type: 'authorization_code',
-          code: req.params.code,
-          redirect_uri,
-          client_id,
-          client_secret
-        }), 
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        }
-      );
-  
-      res.json(response.data);  // send response to client
-      config.accessToken = response.data.access_token
+    const response = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      querystring.stringify({
+        grant_type: 'authorization_code',
+        code: req.params.code,
+        redirect_uri,
+        client_id,
+        client_secret,
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
+
+    res.json(response.data); // send response to client
+    config.accessToken = response.data.access_token;
   } catch (error) {
-      console.error(error.response.data.error);  // log error for debugging
-      res.status(500).json({ error: error.toString() });  // send error to client
+    console.error(error.response.data.error); // log error for debugging
+    res.status(500).json({ error: error.toString() }); // send error to client
   }
 };
 
-module.exports = { authRequest, getAccessToken }
+module.exports = { authRequest, getAccessToken };
